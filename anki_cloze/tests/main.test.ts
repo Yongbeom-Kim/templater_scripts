83edb491368c39d8e9ee8a9dbf435f3b268cdfb5
Kim Yongbeom <dernbu@gmail.com>
2025-04-08 16:22:50 +0800

    refactor(anki_cloze tests): Only test e2e

--- anki_cloze/tests/main.test.ts
+++ anki_cloze/tests/main.test.ts
@@ -1,27 +1,5 @@
 import { clozify } from "../src/main";
-import { tokenize } from "../src/tokenizer/tokenizer";
-import { TestCases as TextTestCases } from "./data/text.testcase";
-import { TestCases as ListTestCases } from "./data/list.testcase";
 import { parseSpecFile } from "./data/testcase_parser";
-// describe("test text.testcase.ts", () => {
-//   it.each(TextTestCases)(
-//     'should tokenize "%p" correctly',
-//     ({ input, main_expect }) => {
-//       const output = clozify(input);
-//       main_expect(output);
-//     },
-//   );
-// });
-
-// describe("test list.testcase.ts", () => {
-//   it.each(ListTestCases)(
-//     'should tokenize "%p" correctly',
-//     ({ input, main_expect }) => {
-//       const output = clozify(input);
-//       main_expect(output);
-//     },
-//   );
-// });
 
 describe("test spec", () => {
   describe("code_blocks", () => {
