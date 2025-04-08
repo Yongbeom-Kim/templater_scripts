2606f14ce130d1a11f771ae29325534d307b2a18
Kim Yongbeom <dernbu@gmail.com>
2025-04-09 04:26:24 +0800

    chore(anki_cloze): Make test case order alphabetical

--- anki_cloze/tests/main.test.ts
+++ anki_cloze/tests/main.test.ts
@@ -3,12 +3,12 @@ import { parseSpecFile } from "./data/testcase_parser";
 
 describe("test spec", () => {
   describe.each([
-    "text/lists.spec.txt",
-    "code_blocks/languages.spec.txt",
-    "code_blocks/indents.spec.txt",
-    "code_blocks/curly_braces.spec.txt",
-    "text/curly_braces.spec.txt",
     "code_blocks/cloze_index.spec.txt",
+    "code_blocks/curly_braces.spec.txt",
+    "code_blocks/indents.spec.txt",
+    "code_blocks/languages.spec.txt",
+    "text/curly_braces.spec.txt",
+    "text/lists.spec.txt",
   ])("%s", (name) => {
     const testCases = parseSpecFile(`tests/data/${name}`);
     it.each(testCases)("$name", ({ input, expected, flags }) => {
